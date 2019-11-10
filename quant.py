8808657377bf3d5feb6ebfd1a6cd3a84ac792356
Anurag Uniyal <anuraguniyal@gmail.com>
2019-11-09 18:58:05 -0800

    init git repo

---
+++ quant.py
@@ -0,0 +1,65 @@
+import sys
+import csv
+import datetime
+
+class Day:
+    """
+    Day represents a day of stock with open/close prices
+    """
+    def __init__(self, date, open, close, high, low):
+        self.date = date
+        self.open = open
+        self.close = close
+        self.high = high
+        self.low = low
+
+    @classmethod
+    def from_row(cls, row):
+        date = datetime.datetime.strptime(row[0], "%Y-%m-%d").date()
+        return Day(date, row[1], row[4], row[2], row[3])
+
+class Put:
+    def __init__(self, expiry, strike, premium):
+        self.expiry = expiry
+        self.strike = strike
+        self.premium = premium
+
+class Strategy:
+    """
+    base class for strategy
+    """
+    def add_day(self, day):
+        # buy put
+        # we assume we buy put at open
+
+
+class BuyPutStrategy:
+    """
+    buy put and wait for collapse
+    """
+    def __init__(self, strike_down, expiry_days):
+        """
+        strike_down: precent below current price at which we buy put
+        expiry_days: in how many days put expires
+        """
+        self.strike_down = strike_down
+        self.expiry_days = expiry_days
+        self.premium = calculate_premium(strike_down, expiry_days)
+        self.puts = []
+
+    def add_day(self, day):
+        pass
+
+def simulate(csv_path, strategy):
+    with open(csv_path) as csv_file:
+        reader = csv.reader(csv_file)
+        for i, row in enumerate(reader):
+            if i == 0:
+                continue # skip header
+            strategy.add_day(Day.from_row(row))
+
+
+strategy = BuyPutStrategy()
+
+simulate(sys.argv[1], strategy)
+
